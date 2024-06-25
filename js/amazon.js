52d9d74da07718d4abf0fe7fd02e1d02bac5a24e
real4real <krisakwamens@gmail.com>
2024-06-25 08:52:29 +0200

    Use products.js file

--- js/amazon.js
+++ js/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-  productImage: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  productName: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  productRating: {
-    stars: 4.5,
-    count: 87
-  },
-  productPriceCents: 1090
-}, {
-  productImage: 'images/products/intermediate-composite-basketball.jpg',
-  productName: 'Intermediate Size Basketball',
-  productRating: {
-    stars: 4.0,
-    count: 127
-  },
-  productPriceCents: 2095
-}, {
-  productImage: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  productName: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  productRating: {
-    stars: 4.5,
-    count: 56
-  },
-  productPriceCents: 799
-}];
-
 let innerHTML = '';
 
 products.forEach((product) => {
@@ -31,23 +5,23 @@ products.forEach((product) => {
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${product.productImage}">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${product.productName}
+            ${product.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.productRating.stars * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.productRating.count}
+              ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.productPriceCents/100).toFixed(2)}
+            $${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -78,6 +52,5 @@ products.forEach((product) => {
         </div>`;
 });
 
-console.log(innerHTML);
 const mainDivElement = document.querySelector('.products-grid');
 mainDivElement.innerHTML = innerHTML;
